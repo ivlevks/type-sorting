ea5e91d69a1d6276724dab5c9b1f9e2bc173463a
ivlevks <ivlevks@yandex.ru>
2022-01-02 16:16:01 +0300

    bubble sort

--- src/ivlevks/Main.java
+++ src/ivlevks/Main.java
@@ -1,8 +1,24 @@
 package ivlevks;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random random = new Random();
+        int array[] = random.ints(10, 0, 100).toArray();
+
+        System.out.print("Начальный массив: ");
+        show(array);
+        System.out.print("Сортированный массив: ");
+        show(Sort.bubbleSort(array));
+    }
+
+    public static void show(int[] array) {
+        Arrays.stream(array).forEach(n -> {
+            System.out.print(n + " ");
+        });
+        System.out.println();
     }
 }
--- src/ivlevks/Sort.java
+++ src/ivlevks/Sort.java
@@ -4,4 +4,19 @@ package ivlevks;
  * IvlevKS
  */
 public class Sort {
+
+    // bubble sorting
+    public static int[] bubbleSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int k = 0; k < i; k++) {
+                if (array[k] > array[k + 1]) {
+                    int temp = array[k];
+                    array[k] = array[k + 1];
+                    array[k + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
 }
